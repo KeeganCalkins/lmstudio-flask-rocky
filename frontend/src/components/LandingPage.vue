993cf394bcbd7af31d24976709de7261dd9598ca
avirathod <rathodavi02@gmail.com>
2025-07-02 01:54:58 -0400

    Adding frontend files

---
+++ frontend/src/components/LandingPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="landing-page">
+    <h1>Welcome to Rocky!</h1>
+    <p>KSU's hometown AI API.</p>
+    <button @click="goToApp">Request a key</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LandingPage',
+  methods: {
+    goToApp() {
+      this.$router.push('/login');
+    }
+  }
+}
+</script>
+
+<style scoped>
+.landing-page {
+  text-align: center;
+  margin-top: 100px;
+}
+button {
+  padding: 10px 20px;
+  font-size: 18px;
+  margin-top: 20px;
+}
+</style>
---
+++ frontend/src/components/LoginPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="login-page">
+    <h1>Login</h1>
+    <p></p>
+    <button >Login with Microsoft</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginPage',
+  methods: {
+    
+  }
+}
+</script>
+
+<style scoped>
+.login-page {
+  text-align: center;
+  margin-top: 100px;
+}
+button {
+  padding: 10px 20px;
+  font-size: 18px;
+  margin-top: 20px;
+}
+</style>
